c7901cefa21cfdb7e44b2b3fb80a7de05dc7a5d5
quangcsp <nguyenvanquangcsp@gmail.com>
2017-09-25 09:23:53 +0700

    realtime_notifications

---
+++ resources/assets/javascripts/listen_event_pusher.js
@@ -0,0 +1,15 @@
+Pusher.logToConsole = true;
+var pusher = new Pusher(configs.pusher.key, {
+  cluster: configs.pusher.cluster,
+  encrypted: configs.pusher.encrypted
+});
+
+var channel = pusher.subscribe('channel_notification');
+channel.bind('App\\Events\\NotificationHandler', function(data) {
+    $('#notification_' + data.user_id).html(parseInt($('#notification_' + data.user_id).html()) + 1);
+    $('#notification_icon_' + data.user_id).html(parseInt($('#notification_icon_' + data.user_id).html())+1);
+    if(data.user_id == $('#get-user-id').html())
+    {
+        showNotify('success', data.messages, {icon: "glyphicon glyphicon-ok"}, {delay: 5000});
+    }
+});
