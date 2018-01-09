3fdefa39084f1be7ba323de9943a5e56a34e5cec
Quang Huy <dqhuy78@gmail.com>
2018-01-09 14:46:19 +0700

    Move request to admin site

--- resources/assets/javascripts/admin/admin.js
+++ resources/assets/javascripts/admin/admin.js
@@ -9,3 +9,50 @@ function showNotify(message, messageType, timer)
         hideAfter: timer
     });
 }
+
+timeAGo = function ( date ) {
+    var seconds = Math.floor(( Date.parse(new Date()) - Date.parse(date) ) / 1000 );
+    interval = Math.floor( seconds / 86400 );
+    if (interval > 30) {
+        return date;
+    }
+    if (interval == 1) {
+
+        return interval + ' day ago';
+    }
+    if (interval > 1) {
+
+        return interval + ' days ago';
+    }
+    interval = Math.floor( seconds / 3600 );
+    if (interval == 1) {
+
+        return interval + ' hour ago';
+    }
+    if (interval > 1) {
+
+        return interval + ' hours ago';
+    }
+    interval = Math.floor( seconds / 60 );
+    if (interval == 1) {
+
+        return interval + ' minute ago';
+    }
+    if (interval > 1) {
+
+        return interval + ' minutes ago';
+    }
+
+    return Math.floor(seconds) + ' seconds ago';
+};
+
+$(document).ready(function() {
+    $('.time_a_go').each(function() {
+        $(this).html(timeAGo($(this).html()));
+    });
+
+    $('._approve-btn').on('click', function() {
+        $(this).addClass('disabled');
+        $(this).html($(this).data('loading'));
+    });
+});
